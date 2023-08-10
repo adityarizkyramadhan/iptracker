0fde03f71049f6d4aaa81a00978473c240fcb85f
adityarizkyramadhan <90701908+adityarizkyramadhan@users.noreply.github.com>
2023-08-10 10:04:00 +0700

    fix(tracer) : extract only main data

--- tracer.go
+++ tracer.go
@@ -1,6 +1,7 @@
 package iptracker
 
 import (
+	"errors"
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
 	"io"
@@ -11,29 +12,22 @@ const (
 	linkWeb = "https://iplogger.org/ip-tracker/?ip="
 )
 
+var (
+	ErrGetData = errors.New("fail to get data")
+)
+
 type IPInfo struct {
-	IPAddress      string
-	Continent      string
-	Country        string
-	City           string
-	Latitude       string
-	Longitude      string
-	Accuracy       string
-	StateRegion    string
-	DistrictCounty string
-	ZipCode        string
-	TimeZone       string
-	LocalTime      string
-	ISPProvider    string
-	Organization   string
-	Connection     string
-	EuropeanUnion  string
-	WeatherStation string
+	IPAddress string
+	Continent string
+	Country   string
+	City      string
+	Latitude  string
+	Longitude string
+	Accuracy  string
 }
 
 func Trace(ip string) (*IPInfo, error) {
 	client := new(http.Client)
-	ipInfo := IPInfo{}
 	req, err := http.NewRequest(http.MethodGet, linkWeb+ip, nil)
 	if err != nil {
 		fmt.Println(err.Error())
@@ -44,6 +38,10 @@ func Trace(ip string) (*IPInfo, error) {
 		fmt.Println(err.Error())
 		return nil, err
 	}
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println(ErrGetData.Error())
+		return nil, ErrGetData
+	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
@@ -52,47 +50,18 @@ func Trace(ip string) (*IPInfo, error) {
 	}(resp.Body)
 	doc, err := goquery.NewDocumentFromReader(resp.Body)
 	if err != nil {
+		fmt.Println(err.Error())
 		return nil, err
 	}
-	doc.Find(".ip-info div").Each(func(i int, element *goquery.Selection) {
-		key := element.Find("div:first-child").Text()
-		value := element.Find("div:last-child").Text()
-		switch key {
-		case "IP address":
-			ipInfo.IPAddress = value
-		case "Continent":
-			ipInfo.Continent = value
-		case "Country by IP":
-			ipInfo.Country = value
-		case "City by IP":
-			ipInfo.City = value
-		case "Latitude":
-			ipInfo.Latitude = value
-		case "Longitude":
-			ipInfo.Longitude = value
-		case "Accuracy":
-			ipInfo.Accuracy = value
-		case "State / Region":
-			ipInfo.StateRegion = value
-		case "District / County":
-			ipInfo.DistrictCounty = value
-		case "Zip code":
-			ipInfo.ZipCode = value
-		case "TimeZone":
-			ipInfo.TimeZone = value
-		case "Local time":
-			ipInfo.LocalTime = value
-		case "ISP / Provider":
-			ipInfo.ISPProvider = value
-		case "Organization":
-			ipInfo.Organization = value
-		case "Connection":
-			ipInfo.Connection = value
-		case "European union":
-			ipInfo.EuropeanUnion = value
-		case "Weather station":
-			ipInfo.WeatherStation = value
-		}
-	})
+	ipInfo := IPInfo{
+		IPAddress: doc.Find(".ip-info div:contains('IP address') .copy").AttrOr("data-copy", ""),
+		Continent: doc.Find(".ip-info div:contains('Continent') .country").Text(),
+		Country:   doc.Find(".ip-info div:contains('Country by IP') .country span").Text(),
+		City:      doc.Find(".ip-info div:contains('City by IP') .country").Text(),
+		Latitude:  doc.Find(".ip-info div:contains('Latitude') .icon span").Text(),
+		Longitude: doc.Find(".ip-info div:contains('Longitude') .icon span").Text(),
+		Accuracy:  doc.Find(".ip-info div:contains('Accuracy') span[data-named='content']").Text(),
+	}
+
 	return &ipInfo, nil
 }
